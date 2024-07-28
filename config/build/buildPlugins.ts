ceea17399c95669e3ce0ae59d5360a0bdb506e92
pozib-hub <pozib@mail.ru>
2024-07-28 21:27:26 +0300

    fix: build production

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -11,22 +11,31 @@ export function buildPlugins({
     paths,
     isDev,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
-    return [
-        new HTMLWebpackPlugin({
-            template: paths.html,
-        }),
-        new webpack.ProgressPlugin(),
-        new MiniCssExtractPlugin({
-            filename: 'css/[name].[contenthash:8].css',
-            chunkFilename: 'css/[name].[contenthash:8].css',
-        }),
-        new webpack.DefinePlugin({
-            __IS_DEV__: JSON.stringify(isDev),
-        }),
-        new ReactRefreshWebpackPlugin(),
-        new webpack.HotModuleReplacementPlugin(),
-        new BundleAnalyzerPlugin({
-            openAnalyzer: false,
-        }),
+    const plugins = [new HTMLWebpackPlugin({
+        template: paths.html,
+    }),
+    new webpack.ProgressPlugin(),
+    new MiniCssExtractPlugin({
+        filename: 'css/[name].[contenthash:8].css',
+        chunkFilename: 'css/[name].[contenthash:8].css',
+    }),
+    new webpack.DefinePlugin({
+        __IS_DEV__: JSON.stringify(isDev),
+    }),
+
     ]
+
+    if (isDev) {
+        const dev_plugins = [
+            new ReactRefreshWebpackPlugin(),
+            new webpack.HotModuleReplacementPlugin(),
+            new BundleAnalyzerPlugin({
+                openAnalyzer: false,
+            }),
+        ]
+
+        plugins.push(...dev_plugins)
+    }
+
+    return plugins
 }
