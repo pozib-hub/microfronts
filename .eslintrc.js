4ed0792c1041deed8a4f7d8d1372b79da97fff4e
pozib-hub <pozib@mail.ru>
2024-07-30 22:43:41 +0300

    fix: add new rules eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,13 +22,14 @@ module.exports = {
         'react',
         '@typescript-eslint',
         'i18next',
+        'react-hooks',
     ],
     rules: {
         camelcase: ['off', { properties: 'never' }],
-        'linebreak-style': 'off',
+        'linebreak-style': ['error', 'windows'],
         'react/jsx-indent': [2, 4],
         'react/jsx-indent-props': [2, 4],
-        indent: [2, 4],
+        indent: ['error', 4],
         'react/jsx-filename-extension': [
             2,
             { extensions: ['.js', '.jsx', '.tsx'] },
@@ -40,10 +41,12 @@ module.exports = {
         'react/react-in-jsx-scope': 'off',
         'react/jsx-props-no-spreading': 'warn',
         'react/function-component-definition': 'off',
+        'jsx-a11y/click-events-have-key-events': 'off',
         'no-shadow': 'off',
         'import/extensions': 'off',
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
+        // 'no-use-before-define': ['error', { functions: false, classes: false, variables: false }],
         semi: ['error', 'never'],
         'max-len': ['error', { code: 100, ignoreComments: true }],
         'no-plusplus': 'off',
@@ -53,15 +56,18 @@ module.exports = {
                 markupOnly: true,
                 ignoreattribute: ['data-testid', 'to'],
             }],
+        'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+        'react-hooks/exhaustive-deps': 'error', // Checks effect dependencies
     },
     globals: {
         __IS_DEV__: true,
     },
     overrides: [
         {
-            files: ['**/src/**/*.test.{ts,tsx}', ''],
+            files: ['**/src/**/*.{test,stories}.{ts,tsx}', ''],
             rules: {
                 'i18next/no-literal-string': 'off',
+                'max-len': 'off',
             },
         },
     ],
