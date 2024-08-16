23b22b7913557c0445acd48adc0632270e20a66d
pozib-hub <pozib@mail.ru>
2024-08-16 13:38:58 +0300

    feat: shared ui Text

---
+++ src/shared/ui/Text/Text.stories.tsx
@@ -0,0 +1,84 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { ThemeDecorator } from 'src/shared/config/storybook/ThemeDecorator/ThemeDecorator'
+import { CenterDecorator } from 'shared/config/storybook/CenterDecorator/CenterDecorator'
+import { Theme } from 'src/app/providers/ThemeProvider'
+import { Text } from './Text'
+
+const meta: Meta<typeof Text> = {
+    title: 'shared/Text',
+    component: Text,
+}
+
+export default meta
+
+type Story = StoryObj<typeof Text>;
+
+export const Default: Story = {
+    args: {
+        children: 'Text',
+    },
+    decorators: [CenterDecorator],
+}
+
+export const h1: Story = {
+    args: {
+        variant: 'h1',
+        children: 'Text',
+    },
+    decorators: [CenterDecorator],
+}
+
+export const Paragraph: Story = {
+    args: {
+        variant: 'p',
+        children: 'Text',
+    },
+    decorators: [CenterDecorator],
+}
+
+export const ParagraphError: Story = {
+    args: {
+        variant: 'p',
+        color: 'red',
+        children: 'Text',
+    },
+    decorators: [CenterDecorator],
+}
+
+export const ParagraphEllipsis: Story = {
+    args: {
+        variant: 'p',
+        ellipsis: true,
+        children: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, quod rem error enim tenetur ab suscipit impedit sint culpa ea.',
+    },
+    decorators: [CenterDecorator],
+}
+
+export const ParagraphEllipsisWith50px: Story = {
+    args: {
+        variant: 'p',
+        ellipsis: {
+            width: 50,
+        },
+        children: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, quod rem error enim tenetur ab suscipit impedit sint culpa ea.',
+    },
+    decorators: [CenterDecorator],
+}
+
+export const ParagraphEllipsisWith50pxIn3Lines: Story = {
+    args: {
+        variant: 'p',
+        ellipsis: {
+            width: 50,
+            lines: 3,
+        },
+        children: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, quod rem error enim tenetur ab suscipit impedit sint culpa ea. Dolor, quod rem error enim tenetur ab suscipit impedit sint culpa ea',
+    },
+    decorators: [CenterDecorator],
+}
+
+// export const DefaultThemeDark: Story = {
+//     ...Default,
+//     decorators: [CenterDecorator, ThemeDecorator(Theme.DARK)],
+// }
---
+++ src/shared/ui/Text/Text.tsx
@@ -0,0 +1,76 @@
+import React, { HTMLAttributes, FC, CSSProperties } from 'react'
+
+import cn from 'shared/lib/classNames/classNames'
+
+import styles from './Text.module.scss'
+
+type Variants = 'span' | 'p' | 'h1' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5'
+type Sizes = 'small' | 'medium' | 'large'
+type Colors = 'primary' | 'secondary' | 'red' | 'white'
+
+interface ITextProps extends HTMLAttributes<HTMLSpanElement> {
+    disabled?: boolean
+    bold?: boolean
+    variant?: Variants
+    size?: Sizes,
+    color?: Colors,
+    ellipsis?: boolean | {
+        lines?: number,
+        width: string | number,
+    }
+    children?: React.ReactNode
+}
+
+export const Text: FC<ITextProps> = (props) => {
+    const {
+        className,
+        children,
+        variant = 'span',
+        size = 'medium',
+        color,
+        disabled,
+        bold,
+        ellipsis,
+        style,
+    } = props
+
+    const classes = [
+        styles.Text,
+        {
+            [styles.disabled]: disabled,
+            [styles.bold]: bold,
+        },
+        styles[color || ""],
+        styles[variant],
+        styles[size],
+        className,
+    ]
+
+    const ellipsis_style: CSSProperties = {}
+
+    if (typeof ellipsis === 'boolean') {
+        ellipsis_style.display = 'inline-block'
+        ellipsis_style.overflow = 'hidden'
+        ellipsis_style.whiteSpace = 'nowrap'
+        ellipsis_style.textOverflow = 'ellipsis'
+    }
+
+    if (typeof ellipsis === 'object') {
+        ellipsis_style.display = '-webkit-box'
+        ellipsis_style.WebkitBoxOrient = 'vertical'
+        ellipsis_style.overflow = 'hidden'
+        ellipsis_style.WebkitLineClamp = ellipsis.lines || 1
+        ellipsis_style.width = ellipsis.width
+    }
+
+    const inline_style = {
+        ...ellipsis_style,
+        ...style,
+    }
+
+    return React.createElement<HTMLAttributes<HTMLSpanElement>>(
+        variant,
+        { ...props, className: cn(classes), style: inline_style },
+        children,
+    )
+}
