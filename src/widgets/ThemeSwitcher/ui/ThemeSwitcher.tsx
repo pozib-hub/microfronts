4e09a7f95cecf0400c6f43c38f383d50e3332540
pozib-hub <pozib@mail.ru>
2024-08-16 13:37:48 +0300

    refactor: ThemeSwitcher

--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -4,7 +4,7 @@ import cn from 'src/shared/lib/classNames/classNames'
 import { useTheme } from 'src/app/providers/ThemeProvider'
 import { Button } from 'src/shared/ui/Button/Button'
 
-import { iconByTheme } from '../utils'
+import { getIconByTheme } from '../utils'
 
 import style from './ThemeSwitcher.module.scss'
 
@@ -16,9 +16,11 @@ export const ThemeSwitcher: FC<IThemeSwitcherProps> = (props) => {
     const { className } = props
     const { theme, toggleTheme } = useTheme()
 
+    const Icon = getIconByTheme(theme)
+
     return (
         <div className={cn(style.className, className)}>
-            <Button onClick={toggleTheme}>{iconByTheme[theme]}</Button>
+            <Button variant='transparent' onClick={toggleTheme}>{<Icon />}</Button>
         </div>
     )
 }
--- src/widgets/ThemeSwitcher/utils.tsx
+++ src/widgets/ThemeSwitcher/utils.tsx
@@ -4,12 +4,24 @@ import { Theme } from 'src/app/providers/ThemeProvider'
 import IconDark from 'src/shared/assets/icons/theme-dark.svg'
 import IconLight from 'src/shared/assets/icons/theme-light.svg'
 
-export const iconByTheme: Record<string, React.ReactNode> = {
-    [Theme.DARK]: <IconDark />,
-    [Theme.LIGHT]: <IconLight />,
+export const getIconByTheme = (theme?: Theme) => {
+    switch (theme) {
+        case Theme.DARK:
+            return IconDark
+        case Theme.LIGHT:
+            return IconLight
+        default:
+            return IconLight
+    }
+}
+export const getInversionIconByTheme = (theme?: Theme) => {
+    switch (theme) {
+        case Theme.DARK:
+            return IconLight
+        case Theme.LIGHT:
+            return IconDark
+        default:
+            return IconLight
+    }
 }
 
-export const inversionIconByTheme: Record<string, React.ReactNode> = {
-    [Theme.DARK]: <IconLight />,
-    [Theme.LIGHT]: <IconDark />,
-}
