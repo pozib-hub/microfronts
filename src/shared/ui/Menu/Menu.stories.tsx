12b602bf8c58a90c7ed0bbb12b27ab59f85eba2d
pozib-hub <pozib@mail.ru>
2024-09-23 17:23:51 +0300

    feat: add menu component

---
+++ src/shared/ui/Menu/Menu.stories.tsx
@@ -0,0 +1,31 @@
+// import React from 'react';
+// import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+// import { Button } from '../Button/Button';
+// import { Menu } from './Menu';
+
+// export default {
+//     title: 'shared/Menu',
+//     component: Dropdown,
+//     argTypes: {
+//         backgroundColor: { control: 'color' },
+//     },
+// } as ComponentMeta<typeof Dropdown>;
+
+// const Template: ComponentStory<typeof Menu> = (args) => <Menu {...args} />;
+
+// export const Normal = Template.bind({});
+// Normal.args = {
+//     trigger: <Button>Open</Button>,
+//     items: [
+//         {
+//             content: 'first',
+//         },
+//         {
+//             content: 'second',
+//         },
+//         {
+//             content: 'third',
+//         },
+//     ],
+// };
