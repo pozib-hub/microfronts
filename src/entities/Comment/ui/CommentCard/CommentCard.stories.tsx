4dc78b4e44398d2e238b5854f0aea9a3b87a6289
pozib-hub <pozib@mail.ru>
2024-11-06 12:10:13 +0300

    feat: add story for CommentCard

--- src/entities/Comment/ui/CommentCard/CommentCard.stories.tsx
+++ src/entities/Comment/ui/CommentCard/CommentCard.stories.tsx
@@ -1,31 +1,36 @@
 import { Meta, StoryObj } from '@storybook/react'
 
-import { ThemeDecorator } from '@shared/config/storybook/ThemeDecorator/ThemeDecorator'
 import { CenterDecorator } from '@shared/config/storybook/CenterDecorator/CenterDecorator'
-import { Theme } from '@shared/const/them'
 import { CommentCard } from './CommentCard'
 
 const meta: Meta<typeof CommentCard> = {
     title: 'entities/CommentCard',
     component: CommentCard,
+    decorators: [CenterDecorator],
 }
 
 export default meta
 
 type Story = StoryObj<typeof CommentCard>;
 
-
-export const Default: Story = {
-    args: {},
-    decorators: [CenterDecorator],
+export const Normal: Story = {
+    args: {
+        comment: {
+            articleId: "1",
+            id: "1",
+            text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. soluta corporis autem quod!",
+            user: {
+                id: "1",
+                username: "John Doe",
+                avatar: "https://example.com/avatar.jpg"
+            }
+        },
+        isLoading: false
+    },
 }
 
-export const Primary: Story = {
-    args: {},
-    decorators: [CenterDecorator],
-}
-
-export const DefaultThemeDark: Story = {
-    ...Default,
-    decorators: [CenterDecorator, ThemeDecorator(Theme.DARK)],
+export const Loading: Story = {
+    args: {
+        isLoading: true
+    },
 }
