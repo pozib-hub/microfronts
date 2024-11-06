a165afdaed2a026e6b15add190bd66c7f0b4f300
pozib-hub <pozib@mail.ru>
2024-11-06 10:19:39 +0300

    feat: add filled and default styles and sizes Input

--- src/shared/ui/Input/Input.stories.tsx
+++ src/shared/ui/Input/Input.stories.tsx
@@ -1,45 +1,157 @@
-import { Meta, StoryObj } from '@storybook/react'
+import { Meta, StoryFn, StoryObj } from '@storybook/react'
+import { useState } from 'react'
 
-import { ThemeDecorator } from '@shared/config/storybook/ThemeDecorator/ThemeDecorator'
 import { CenterDecorator } from '@shared/config/storybook/CenterDecorator/CenterDecorator'
-import { Theme } from '@shared/const/them'
-import { Input } from './Input'
+import { Input, IInputProps } from './Input'
+import { Icon } from '../Icon/Icon'
 
 const meta: Meta<typeof Input> = {
     title: 'shared/Input',
     component: Input,
+    argTypes: {
+        value: { control: "text" }
+    },
+    decorators: [CenterDecorator],
 }
 
 export default meta
 
 type Story = StoryObj<typeof Input>;
 
-export const InputDefault: Story = {
-    args: {
-        label: 'label',
-        value: 'text',
-    },
-    decorators: [CenterDecorator],
+const Template: StoryFn<IInputProps> = (args) => {
+    const [value, setValue] = useState(args.value || '')
+
+    return (
+        <Input
+            {...args}
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+        />
+    )
 }
 
-export const InputFilled: Story = {
-    args: {
-        label: 'label',
-        variant: 'filled',
-        value: 'text',
-    },
-    decorators: [CenterDecorator],
+// Standard ------------
+
+export const Standard = Template.bind({})
+Standard.args = {
 }
 
-export const InputOutline: Story = {
-    args: {
-        label: 'label',
-        variant: 'outline',
-        value: 'text',
-    },
-    decorators: [CenterDecorator],
+export const StandardWithLabel = Template.bind({})
+StandardWithLabel.args = {
+    label: 'label',
 }
 
-export const InputThemeDark: Story = {
-    decorators: [CenterDecorator, ThemeDecorator(Theme.DARK)],
+export const StandardWithLabelAndPlaceholder = Template.bind({})
+StandardWithLabelAndPlaceholder.args = {
+    label: 'label',
+    placeholder: 'placeholder',
+}
+
+export const StandardDisabled = Template.bind({})
+StandardDisabled.args = {
+    label: 'label',
+    disabled: true,
+}
+
+export const StandardError = Template.bind({})
+StandardError.args = {
+    label: 'label',
+    required: true,
+    error: true,
+    errorMessage: "Что то пошло не так"
+}
+
+export const StandardWithIcons = Template.bind({})
+StandardWithIcons.args = {
+    label: 'label',
+    iconLeft: <Icon id='Eye' size={20} />,
+    iconRight: <Icon id='Notification' size={18} />
+}
+
+// Filled ------------
+
+export const Filled = Template.bind({})
+Filled.args = {
+    variant: 'filled',
+}
+
+export const FilledWithLabel = Template.bind({})
+FilledWithLabel.args = {
+    label: 'label',
+    variant: 'filled',
+}
+
+export const FilledWithLabelAndPlaceholder = Template.bind({})
+FilledWithLabelAndPlaceholder.args = {
+    label: 'label',
+    variant: 'filled',
+    placeholder: 'placeholder',
+}
+
+export const FilledDisabled = Template.bind({})
+FilledDisabled.args = {
+    label: 'label',
+    variant: 'filled',
+    disabled: true,
+}
+
+export const FilledError = Template.bind({})
+FilledError.args = {
+    label: 'label',
+    variant: 'filled',
+    required: true,
+    error: true,
+    errorMessage: "Что то пошло не так"
+}
+
+export const FilledWithIcons = Template.bind({})
+FilledWithIcons.args = {
+    label: 'label',
+    variant: 'filled',
+    iconLeft: <Icon id='Eye' size={20} />,
+    iconRight: <Icon id='Notification' size={18} />
+}
+
+// Outline ------------
+
+export const Outline = Template.bind({})
+Outline.args = {
+    variant: 'outline',
+}
+
+export const OutlineWithLabel = Template.bind({})
+OutlineWithLabel.args = {
+    label: 'label',
+    variant: 'outline',
+}
+
+export const OutlineWithLabelAndPlaceholder = Template.bind({})
+OutlineWithLabelAndPlaceholder.args = {
+    label: 'label',
+    variant: 'outline',
+    placeholder: 'placeholder',
+}
+
+export const OutlineDisabled = Template.bind({})
+OutlineDisabled.args = {
+    label: 'label',
+    variant: 'outline',
+    disabled: true,
+}
+
+export const OutlineError = Template.bind({})
+OutlineError.args = {
+    label: 'label',
+    variant: 'outline',
+    required: true,
+    error: true,
+    errorMessage: "Что то пошло не так"
+}
+
+export const OutlineWithIcons = Template.bind({})
+OutlineWithIcons.args = {
+    label: 'label',
+    variant: 'outline',
+    iconLeft: <Icon id='Eye' size={20} />,
+    iconRight: <Icon id='Notification' size={18} />
 }
--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -11,8 +11,8 @@ import cn from '@shared/lib/classNames/classNames'
 import styles from './Input.module.scss'
 
 type Variants = 'filled' | 'outline' | 'default'
-
-interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {
+type Sizes = 'small' | 'medium' | 'large'
+export interface IInputProps extends Omit<InputHTMLAttributes<HTMLInputElement>, "size"> {
     variant?: Variants
     classNameWrapper?: string
     label?: string
@@ -21,6 +21,9 @@ interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {
     errorMessage?: string
     error?: boolean
     autoFocus?: boolean
+    disabled?: boolean
+    placeholder?: string
+    size?: Sizes
 }
 
 export const Input: FC<IInputProps> = memo(function Input(props) {
@@ -35,10 +38,13 @@ export const Input: FC<IInputProps> = memo(function Input(props) {
         error,
         style,
         autoFocus,
+        disabled,
+        placeholder,
+        size = "medium",
         ...inputProps
     } = props
 
-    const isLabelWithPlaceholder = Boolean(label) && Boolean(props.placeholder)
+    const isLabelWithPlaceholder = Boolean(label) && Boolean(placeholder)
 
     const [isFocus, setFocus] = useState(false)
     const ref = useRef<HTMLInputElement>(null)
@@ -54,258 +60,65 @@ export const Input: FC<IInputProps> = memo(function Input(props) {
         <div className={cn(styles.wrapper, classNameWrapper)} style={style}>
             <div className={cn(
                 styles.container,
+                styles[`variant-${variant}`],
+                styles[size],
                 {
+                    [styles.disabled]: disabled,
                     [styles.error]: error,
-                    [styles['variant-default']]: variant === 'default',
-                    [styles['variant-outline']]: variant === 'outline',
-                    [styles['variant-filled']]: variant === 'filled',
                     [styles['label-static']]: isLabelWithPlaceholder,
                 },
             )}
             >
-                <input
-                    ref={ref}
-                    type={type}
-                    {...inputProps}
-                    className={cn(
-                        styles.input,
-                        props.className,
-                        { [styles['not-empty']]: Boolean(props.value) },
-                    )}
-                    style={{}}
-                />
-                {label && !props.hidden && (
-                    <label
-                        className={cn(styles.label)}
-                    >
-                        <span className={styles['label-text']}>{label}</span>
-                        {props.required && <span className={styles['label-star-required']}>∗</span>}
-                    </label>
-                )}
-                <fieldset
-                    aria-hidden="true"
-                    className={styles.fieldset}
-                    style={iconLeft ? { padding: '0 32px' } : {}}
-                >
-                    <legend
-                        className={styles.legend}
-                        style={label ? undefined : { width: '0.01px' }}
-                    >
-                        <span className={styles['label-text']}>{label}</span>
-                        {props.required && <span className={styles['label-required']}>∗</span>}
-                    </legend>
-                </fieldset>
-                {iconRight && <div className={styles['icon-right']}>{iconRight}</div>}
                 {iconLeft && <div className={styles['icon-left']}>{iconLeft}</div>}
+                <div className={styles["container-input"]}
+                >
+                    <input
+                        ref={ref}
+                        type={type}
+                        {...inputProps}
+                        placeholder={placeholder}
+                        disabled={disabled}
+                        className={cn(
+                            styles.input,
+                            props.className,
+                            { [styles['not-empty']]: Boolean(props.value) },
+                        )}
+                        style={{}}
+                    />
+                    {label && !props.hidden && (
+                        <label
+                            className={cn(styles.label)}
+                        >
+                            <span className={styles['label-text']}>{label}</span>
+                            {props.required &&
+                                <span className={styles['label-star-required']}>∗</span>}
+                        </label>
+                    )}
+                    {variant === "outline" && (
+                        <fieldset
+                            aria-hidden="true"
+                            className={styles.fieldset}
+                            style={iconLeft ? { padding: '0 32px' } : {}}
+                        >
+                            <legend
+                                className={styles.legend}
+                                style={label ? undefined : { width: '0.01px' }}
+                            >
+                                <span className={styles['label-text']}>{label}</span>
+                                {props.required && (
+                                    <span className={styles['label-required']}>∗</span>
+                                )}
+                            </legend>
+                        </fieldset>)
+                    }
+                </div>
+                {iconRight && <div className={styles['icon-right']}>{iconRight}</div>}
             </div>
-            {errorMessage && <span className="error-message" title={errorMessage} />}
+            {errorMessage && (
+                <p className={styles['error-message']} title={errorMessage}>
+                    {errorMessage}
+                </p>)
+            }
         </div>
     )
 })
-
-
-// export interface IPropsInput {
-// defaultValue?: string
-// label?: string
-// width?: string
-// placeholder?: string
-// value?: string | number
-// onChange?: ((event: React.ChangeEvent<HTMLInputElement>) => void) | undefined
-// onBlur?: ((event: React.FormEvent<HTMLInputElement>) => void) | undefined
-// onFocus?: ((event: React.FormEvent<HTMLInputElement>) => void) | undefined
-// onKeyDown?: ((event: React.KeyboardEvent<HTMLInputElement>) => void) | undefined
-// type?: string
-// isDisabled?: boolean
-// isRequired?: boolean
-// iconLeft?: React.ReactNode
-// iconRight?: React.ReactNode
-// className?: string
-// margin?: string
-// marginRight?: string
-// error?: boolean
-// isBorderDelete?: boolean
-// autoFocus?: boolean
-// id?: string
-// hidden?: boolean
-// accept?: string
-// isGrayBackground?: boolean
-// isGrayText?: boolean
-// min?: string | number
-// max?: string | number
-// variant?: 'normal' | 'wide' | 'custom'
-// isUnsignedIntegerOnly?: boolean
-// rightContextForWide?: string | React.ReactNode
-// rightContext?: string | React.ReactNode
-// pattern?: string
-// maxLength?: number
-// errorMessage?: string
-// disableWheel?: boolean
-// step?: string
-// successMessage?: string
-// successIcon?: React.ReactNode
-// }
-
-// export const Input = forwardRef<HTMLInputElement, IPropsInput>((props, ref) => {
-//     const {
-//         min,
-//         max,
-//         label,
-//         width,
-//         placeholder,
-//         value,
-//         onChange,
-//         onBlur,
-//         onFocus,
-//         onKeyDown,
-//         type,
-//         isDisabled,
-//         isRequired = false,
-//         iconLeft,
-//         iconRight,
-//         className,
-//         margin,
-//         error,
-//         isBorderDelete,
-//         marginRight,
-//         autoFocus,
-//         hidden,
-//         id,
-//         variant = 'normal',
-//         isGrayBackground = false,
-//         isGrayText = false,
-//         accept,
-//         isUnsignedIntegerOnly,
-//         rightContextForWide,
-//         rightContext,
-//         pattern,
-//         maxLength,
-//         defaultValue,
-//         errorMessage,
-//         disableWheel = false,
-//         step,
-//         successMessage,
-//         successIcon,
-//     } = props
-
-//     const inputEl = useRef<HTMLInputElement>(null)
-
-//     const hdlOnChange = (event: any, onChange: any) => {
-//         const mutatedValue = event.target.value.replace(/[^0-9]/g, '')
-//         onChange({ ...event, target: { ...event.target, value: mutatedValue } })
-//     }
-
-//     useEffect(() => {
-//         const inputElem = inputEl.current
-
-//         const handleWheel = (e: any) => {
-//             e.preventDefault()
-//         }
-
-//         if (disableWheel) {
-//             inputElem?.addEventListener('wheel', handleWheel, { passive: false })
-//         }
-
-//         return () => {
-//             inputElem?.removeEventListener('wheel', handleWheel)
-//         }
-//     }, [])
-
-//     const labelWithPlaceholder = Boolean(label && placeholder)
-//     const styleLabelText: CSSProperties = {}
-
-//     const paddingIfExistIcon = iconLeft ? 40 : undefined
-
-//     if (iconLeft) {
-//         styleLabelText.left = '25px'
-//         styleLabelText.maxWidth = `calc(100% - ${paddingIfExistIcon}px)`
-//     }
-
-//     if (iconLeft && iconRight && paddingIfExistIcon) {
-//         styleLabelText.maxWidth = `calc(100% - ${paddingIfExistIcon * 2}px)`
-//     }
-
-//     return (
-//         <div className={styles['input-container']} style={{ marginBottom: `${margin}`, marginRight: `${marginRight}`, width: `${width}` }}>
-//             <div className={styles.input}>
-//                 <input
-//                     accept={accept}
-//                     hidden={hidden}
-//                     pattern={pattern}
-//                     id={id}
-//                     defaultValue={defaultValue}
-//                     style={iconLeft && iconRight ? { paddingInline: paddingIfExistIcon } : { paddingLeft: paddingIfExistIcon }}
-//                     className={classNames(
-//                         styles.inputField,
-//                         {
-//                             [styles.inputField__error]: error,
-//                             [styles['inputField-normal']]: variant === 'normal',
-//                             [styles['inputField-wide']]: variant === 'wide',
-//                             [styles['inputField-custom']]: variant === 'custom',
-//                         },
-//                         className,
-//                         {
-//                             [styles.inputField__withoutBorder]: isBorderDelete,
-//                             [styles.inputField__gray]: isGrayBackground,
-//                             [styles.inputField__gray_text]: isGrayText,
-//                         },
-//                     )}
-//                     ref={mergeRefs([ref, inputEl])}
-//                     onChange={isUnsignedIntegerOnly ? (e) => hdlOnChange(e, onChange) : onChange}
-//                     onFocus={onFocus}
-//                     onBlur={onBlur}
-//                     onKeyDown={onKeyDown}
-//                     type={type}
-//                     step={step}
-//                     disabled={isDisabled}
-//                     required={isRequired}
-//                     placeholder={placeholder || ' '}
-//                     value={value}
-//                     autoFocus={autoFocus}
-//                     min={min}
-//                     max={max}
-//                     maxLength={maxLength}
-//                 />
-//                 {variant !== 'custom' && label && !hidden && (
-//                     <label
-//                         className={classNames(styles.input__label, {
-//                             [styles['input__label-static']]: labelWithPlaceholder,
-//                         })}
-//                         style={styleLabelText}
-//                     >
-//                         <span className={styles['input__label-text']}>{label}</span>
-//                         {isRequired && <span className={styles['input__label-required']}>∗</span>}
-//                     </label>
-//                 )}
-//                 {variant !== 'custom' && !hidden && (
-//                     <fieldset aria-hidden="true" className={styles.input__fieldset} style={iconLeft ? { padding: '0 32px' } : {}}>
-//                         <legend className={styles.input__legend} style={label ? undefined : { width: '0.01px' }}>
-//                             <span className={styles['input__label-text']}>{label}</span>
-//                             {isRequired && <span className={styles['input__label-required']}>∗</span>}
-//                         </legend>
-//                     </fieldset>
-//                 )}
-
-//                 {variant === 'normal' && !hidden && (
-//                     <>
-//                         {!iconRight && rightContext && (
-//                             <div className={classNames(styles['input-icon'], [styles['input-icon-right-context']])}>{rightContext}</div>
-//                         )}
-//                         {iconRight && <div className={classNames(styles['input-icon'], [styles['input-icon-right']])}>{iconRight}</div>}
-//                         {iconLeft && <div className={classNames(styles['input-icon'], [styles['input-icon-left']])}>{iconLeft}</div>}
-//                     </>
-//                 )}
-//                 {variant === 'wide' && !hidden && (
-//                     <div className={classNames(styles['input-icon'], [styles['input-icon-right-wide']])}>{rightContextForWide}</div>
-//                 )}
-//             </div>
-//             {errorMessage && <Text className="error" title={errorMessage} type="caption" color="error" />}
-//             {successMessage
-// && (
-//     <div className={styles['input__success-message']}>
-//         {successIcon && successIcon}
-//         <Text className="success" title={successMessage} type="caption" color="secondaryDark" />
-//     </div>
-// )}
-//         </div>
-//     )
-// })
