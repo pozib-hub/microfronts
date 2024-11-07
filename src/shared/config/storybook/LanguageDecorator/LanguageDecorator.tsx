eafa8e0fb4613ca1c6bbf13bbf69d37306d42b55
pozib-hub <pozib@mail.ru>
2024-11-07 19:09:02 +0300

    feat: add LanguageDecorator

---
+++ src/shared/config/storybook/LanguageDecorator/LanguageDecorator.tsx
@@ -0,0 +1,29 @@
+import { Suspense, useEffect } from 'react'
+import { I18nextProvider } from 'react-i18next'
+import { StoryFn, StoryContext } from '@storybook/react'
+
+import i18n from '../../i18n/i18n'
+
+i18n.on("languageChanged", (locale) => {
+    const direction = i18n.dir(locale)
+    document.dir = direction
+})
+
+export const LanguageDecorator = (Story: StoryFn, context: StoryContext) => {
+    const { local } = context.globals
+
+    useEffect(() => {
+        i18n.changeLanguage(local)
+
+    }, [local])
+
+    console.log(i18n)
+
+
+
+    return <Suspense fallback={<div>loading translations...</div>}>
+        <I18nextProvider i18n={i18n}>
+            <Story />
+        </I18nextProvider>
+    </Suspense>
+}
