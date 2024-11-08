2ccb2f0006cf6dd7ea14db6ec1f4e3cb8b20533c
pozib-hub <pozib@mail.ru>
2024-11-08 17:15:19 +0300

    feat: change styles NotFoundPage

--- src/pages/NotFoundPage/ui/NotFoundPage.stories.tsx
+++ src/pages/NotFoundPage/ui/NotFoundPage.stories.tsx
@@ -1,7 +1,5 @@
 import { Meta, StoryObj } from '@storybook/react'
 
-import { ThemeDecorator } from '@shared/config/storybook/ThemeDecorator/ThemeDecorator'
-import { Theme } from '@shared/const/them'
 import NotFoundPage from './NotFoundPage'
 
 const meta: Meta<typeof NotFoundPage> = {
@@ -13,9 +11,4 @@ export default meta
 
 type Story = StoryObj<typeof NotFoundPage>;
 
-export const LightTheme: Story = {
-}
-
-export const DarkTheme: Story = {
-    decorators: [ThemeDecorator(Theme.DARK)],
-}
+export const Default: Story = {}
--- src/pages/NotFoundPage/ui/NotFoundPage.tsx
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -4,6 +4,11 @@ import { useTranslation } from 'react-i18next'
 import cn from '@shared/lib/classNames/classNames'
 
 import styles from './NotFoundPage.module.scss'
+import { Text } from '@shared/ui/Text/Text'
+import { Button } from '@shared/ui/Button/Button'
+import { AppLink } from '@shared/ui/AppLink/AppLink'
+import { routePath } from '@shared/const/router'
+import { Flex } from '@shared/ui/Stack/Flex/Flex'
 
 interface INotFoundPage {
     className?: string
@@ -12,9 +17,17 @@ interface INotFoundPage {
 const NotFoundPage = (props: INotFoundPage) => {
     const { className } = props
 
-    const { t } = useTranslation()
+    const { t } = useTranslation("pages")
 
-    return <div className={cn(styles.page, className)}>{t('notFoundPage')}</div>
+    return <div className={cn(styles.wrapper, className)}>
+        <Text variant='h1'>{t('notFound.title')}</Text>
+        <Flex direction='row' gap='8'>
+            <Text>{t('notFound.subtitle')}</Text>
+            <AppLink to={routePath.main} className={styles.link}>
+                {t("notFound.btnRedirect")}
+            </AppLink>
+        </Flex>
+    </div>
 }
 
 export default NotFoundPage
