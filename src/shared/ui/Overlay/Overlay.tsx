4ebfc75d93f74630b9e6771eebb12d3fa03c435e
pozib-hub <pozib@mail.ru>
2024-12-03 13:50:03 +0300

    feat: add Alerts

--- src/shared/ui/Overlay/Overlay.tsx
+++ src/shared/ui/Overlay/Overlay.tsx
@@ -6,11 +6,17 @@ import styles from './Overlay.module.scss'
 
 interface OverlayProps {
     className?: string
+    clickable?: boolean
     onClick?: () => void
 }
 
 export const Overlay = memo((props: OverlayProps) => {
-    const { className, onClick } = props
+    const { className, clickable = true, onClick } = props
 
-    return <div onClick={onClick} className={cn(styles.Overlay, className)} />
+    return (
+        <div
+            onClick={onClick}
+            className={cn(styles.wrapper, { [styles.clickable]: clickable }, className)}
+        />
+    )
 })
